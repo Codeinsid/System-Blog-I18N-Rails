0c42971be9da67ca737779d6d7a601da0eabea48
Thiago Almeida <103385882+Codeinsid@users.noreply.github.com>
2023-02-16 14:51:48 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resources :especialidadades
   resources :mining_types
   get 'welcome/index'
+  
   resources :cdzs
   
-  root to: 'welcome#index'
+  #root to: 'welcome#index'
+  root to: index.html
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
