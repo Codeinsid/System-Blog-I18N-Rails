1c8167946fe656bd67a4436dbcf066f6e3feafa3
Thiago Almeida <103385882+Codeinsid@users.noreply.github.com>
2023-02-16 14:54:56 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   
   resources :cdzs
   
-  #root to: 'welcome#index'
-  root to: 'index.html'
+  root to: 'welcome#index'
+  #root to: 'index.html'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
